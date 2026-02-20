ef9fcea1b5ea27ae05043f172538925e27d6e380
Yigit Soylu <ysssoylu@gmail.com>
2026-02-20 11:05:16 +0300

    feat: add environment and configurable currenct

--- js-monorepo/apps/expense-tracker/src/app/transactions/dashboard/dashboard.component.ts
+++ js-monorepo/apps/expense-tracker/src/app/transactions/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { TransactionService } from '../transaction.service';
+import { environment } from '@env';
 
 @Component({
   selector: 'expense-tracker-dashboard',
@@ -11,4 +12,7 @@ import { TransactionService } from '../transaction.service';
 })
 export class DashboardComponent {
   protected readonly txService = inject(TransactionService);
+  protected readonly currency = environment.currency;
+  protected readonly currencySymbol = environment.currencySymbol;
+  protected readonly currencyDecimalPlaces = `1.${environment.currencyDecimalPlaces}-${environment.currencyDecimalPlaces}`;
 }
---
+++ js-monorepo/apps/expense-tracker/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  currency: 'TRY',
+  currencySymbol: '₺',
+  currencyDecimalPlaces: 2,
+};
