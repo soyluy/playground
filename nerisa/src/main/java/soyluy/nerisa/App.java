c8e089bd03407426c204d2a3488c7fd9a1582b03
soyluy <ysssoylu@gmail.com>
2026-03-22 21:06:09 +0300

    feat: maven build

--- nerisa/src/main/java/soyluy/nerisa/App.java
+++ nerisa/src/main/java/soyluy/nerisa/App.java
@@ -1,20 +1,42 @@
 package soyluy.nerisa;
 
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-/**
- * Hello world!
- *
- */
 public class App extends Application {
-		@Override
-		public void start(Stage stage){
-			Scene scene = new Scene(new Label("Hello"), 400, 300);
-			stage.setScene(scene);
-			stage.show();
-		}
+	private static final String EMPTY_TEXT = "Please set a date";
+
+	@Override
+	public void start(Stage stage){
+		
+		DatePicker datePicker = new DatePicker();
+
+		Label lbl = new Label("Select a date");
+
+		datePicker.setOnAction(e -> {
+			LocalDate picked = datePicker.getValue();
+			if(picked == null){
+				lbl.setText(EMPTY_TEXT);
+				return;
+			}
+			int c = picked.get(WeekFields.ISO.weekOfWeekBasedYear());
+			lbl.setText("Week " + c);
+		});
+
+		VBox root = new VBox(10);
+		root.getChildren().addAll(datePicker,  lbl);
+		
+		Scene scene = new Scene(root, 400, 300);
+		stage.setScene(scene);
+		stage.show();
+	}
+
 }
