0777e2b1fe367fc2f61056201118453220598bd3
Yigit Soylu <ysssoylu@gmail.com>
2026-03-31 14:21:44 +0300

    feat: add text color based on color brightness

--- js-monorepo/libs/todo/data/src/lib/types/todo-tag.interface.ts
+++ js-monorepo/libs/todo/data/src/lib/types/todo-tag.interface.ts
@@ -1,5 +1,5 @@
 export interface TodoTag {
   id: number;
   name: string;
-  colorHex: string;
+  colorHex: string; // 6-digit hex color code (without the # prefix)
 }
--- js-monorepo/libs/todo/ui/src/lib/components/tag-pill/tag-pill.ts
+++ js-monorepo/libs/todo/ui/src/lib/components/tag-pill/tag-pill.ts
@@ -18,4 +18,13 @@ export class TagPillComponent {
   protected get name(): string {
     return this.tag().name;
   }
+
+  protected get textColor(): string {
+    const color = this.color;
+    const r = parseInt(color.slice(0, 2), 16);
+    const g = parseInt(color.slice(2, 4), 16);
+    const b = parseInt(color.slice(4, 6), 16);
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness > 128 ? '#000' : '#fff';
+  }
 }
