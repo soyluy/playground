b54263b8ad58a96e4d5b01e00bdc464ed3d86889
Yigit Soylu <ysssoylu@gmail.com>
2026-04-03 11:35:11 +0300

    feat: implement a more robust prisma service

--- js-monorepo/libs/prisma/src/lib/services/prisma.service.ts
+++ js-monorepo/libs/prisma/src/lib/services/prisma.service.ts
@@ -1,15 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super({
       adapter: new PrismaPg({
         connectionString: process.env['DATABASE_URL'],
       }),
     });
-    this.$connect();
+  }
+  async onModuleInit(): Promise<void> {
+    console.log('Connecting to database...');
+    try {
+      await this.$connect();
+      console.log('Connected to database');
+    } catch (error) {
+      console.error('Error connecting to database', error);
+      throw error;
+    }
+  }
+  async onModuleDestroy(): Promise<void> {
+    console.log('Disconnecting from database...');
+    try {
+      await this.$disconnect();
+      console.log('Disconnected from database');
+    } catch (error) {
+      console.error('Error disconnecting from database', error);
+      throw error;
+    }
   }
 }
