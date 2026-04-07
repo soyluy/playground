e373bf494a66baf16f5d7d44055e43b0c96ebc5b
Yigit Soylu <ysssoylu@gmail.com>
2026-04-07 17:22:03 +0300

    refactor: simplify array query parameter parsing logic

--- js-monorepo/libs/todo/api/src/lib/common/util/array-query-param-parser.ts
+++ js-monorepo/libs/todo/api/src/lib/common/util/array-query-param-parser.ts
@@ -1,18 +1,9 @@
 export function parseArrayQueryParam<T>(
   value: string | string[],
-  apply?: (value: string) => T,
+  apply: (value: string) => T,
 ): T[] {
   if (Array.isArray(value)) {
-    if (apply) {
-      return value.map(apply);
-    } else {
-      return value.map((v) => v as unknown as T);
-    }
-  } else {
-    if (apply) {
-      return [apply(value)];
-    } else {
-      return [value as unknown as T];
-    }
+    return value.map(apply);
   }
+  return [apply(value)];
 }
