ae206d44664b6db56a705e2196485a8f0abd439d
Yigit Soylu <ysssoylu@gmail.com>
2026-04-07 15:42:24 +0300

    fix: add error handling to http requests

--- js-monorepo/libs/todo/ui/src/lib/services/todo-tag.service.ts
+++ js-monorepo/libs/todo/ui/src/lib/services/todo-tag.service.ts
@@ -30,29 +30,49 @@ export class TodoTagService {
 
   public addTag(tag: CreateTodoTagDto): void {
     const res$ = this._tagApiService.createTag(tag);
-    res$.subscribe((res: CreateTodoTagResponse) => {
-      this._tags.update((tags) => [...tags, res]);
+    res$.subscribe({
+      next: (res: CreateTodoTagResponse) => {
+        this._tags.update((tags) => [...tags, res]);
+      },
+      error: (error) => {
+        console.error('error adding tag', error);
+      },
     });
   }
 
   public updateTag(id: number, tag: UpdateTodoTagDto): void {
     const res$ = this._tagApiService.updateTag(id, tag);
-    res$.subscribe((res: UpdateTodoTagResponse) => {
-      this._tags.update((tags) => tags.map((t) => (t.id === id ? res : t)));
+    res$.subscribe({
+      next: (res: UpdateTodoTagResponse) => {
+        this._tags.update((tags) => tags.map((t) => (t.id === id ? res : t)));
+      },
+      error: (error) => {
+        console.error('error updating tag', error);
+      },
     });
   }
 
   public deleteTag(id: number): void {
     const res$ = this._tagApiService.deleteTag(id);
-    res$.subscribe((res: DeleteTodoTagResponse) => {
-      this._tags.update((tags) => tags.filter((t) => t.id !== res.id));
+    res$.subscribe({
+      next: (res: DeleteTodoTagResponse) => {
+        this._tags.update((tags) => tags.filter((t) => t.id !== res.id));
+      },
+      error: (error) => {
+        console.error('error deleting tag', error);
+      },
     });
   }
 
   private async loadTags() {
     const tags$ = this._tagApiService.getTags();
-    tags$.subscribe((tags) => {
-      this._tags.set(tags);
+    tags$.subscribe({
+      next: (tags) => {
+        this._tags.set(tags);
+      },
+      error: (error) => {
+        console.error('error loading tags', error);
+      },
     });
   }
 }
--- js-monorepo/libs/todo/ui/src/lib/services/todo.service.ts
+++ js-monorepo/libs/todo/ui/src/lib/services/todo.service.ts
@@ -16,34 +16,44 @@ import { debounceTime, switchMap } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class TodoService {
   private readonly _persistenceService = inject(TodoPersistenceService);
-	private readonly _filterService = inject(TodoFilterService);
-	private readonly _activeFilter: Signal<TodoFilter | null> = this._filterService.getFilter();
+  private readonly _filterService = inject(TodoFilterService);
+  private readonly _activeFilter: Signal<TodoFilter | null> =
+    this._filterService.getFilter();
 
-	private readonly _refresh = signal(0);
+  private readonly _refresh = signal(0);
 
   private readonly _todos: Signal<TodoItem[]>;
 
   constructor() {
-    const todos$ = toObservable(computed(()  => ({
-			filter: this._activeFilter(),
-			_refresh: this._refresh(),
-		}))).pipe(
-			debounceTime(300),
-			switchMap(({filter}) => this._persistenceService.getTodos(filter ?? undefined)),
-		);
+    const todos$ = toObservable(
+      computed(() => ({
+        filter: this._activeFilter(),
+        _refresh: this._refresh(),
+      })),
+    ).pipe(
+      debounceTime(300),
+      switchMap(({ filter }) =>
+        this._persistenceService.getTodos(filter ?? undefined),
+      ),
+    );
 
-		this._todos = toSignal(todos$, { initialValue: [] }) as Signal<TodoItem[]>;
+    this._todos = toSignal(todos$, { initialValue: [] }) as Signal<TodoItem[]>;
   }
 
-	public getTodos(): Signal<TodoItem[]> {
-		return this._todos;
-	}
+  public getTodos(): Signal<TodoItem[]> {
+    return this._todos;
+  }
 
   public addTodo(todo: NewTodoItem) {
     const res$ = this._persistenceService.saveTodo(todo);
-    res$.subscribe((res: CreateTodoResponse) => {
-      this._refreshTodos();
-			console.log('todo added', res);
+    res$.subscribe({
+      next: (res: CreateTodoResponse) => {
+        this._refreshTodos();
+        console.log('todo added', res);
+      },
+      error: (error) => {
+        console.error('error adding todo', error);
+      },
     });
   }
 
@@ -57,17 +67,27 @@ export class TodoService {
       tagIds: todo.tags !== null ? todo.tags.map((t) => t.id) : null,
     };
     const res$ = this._persistenceService.updateTodo(dto);
-    res$.subscribe((res: UpdateTodoResponse) => {
-      this._refreshTodos();
-			console.log('todo updated', res);
+    res$.subscribe({
+      next: (res: UpdateTodoResponse) => {
+        this._refreshTodos();
+        console.log('todo updated', res);
+      },
+      error: (error) => {
+        console.error('error updating todo', error);
+      },
     });
   }
 
   public deleteTodo(id: number) {
     const res$ = this._persistenceService.deleteTodo(id);
-    res$.subscribe((res: DeleteTodoResponse) => {
-      this._refreshTodos();
-			console.log('todo deleted', res);
+    res$.subscribe({
+      next: (res: DeleteTodoResponse) => {
+        this._refreshTodos();
+        console.log('todo deleted', res);
+      },
+      error: (error) => {
+        console.error('error deleting todo', error);
+      },
     });
   }
 
@@ -75,7 +95,7 @@ export class TodoService {
     return this._todos().find((t) => t.id === id);
   }
 
-	private _refreshTodos() {
-		this._refresh.update((refresh) => refresh + 1);
-	}
+  private _refreshTodos() {
+    this._refresh.update((refresh) => refresh + 1);
+  }
 }
