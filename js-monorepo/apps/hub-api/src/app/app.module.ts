66bd5bddc29b444875aa7ef7cc171f7a1e3cf5bd
Yigit Soylu <ysssoylu@gmail.com>
2026-04-07 16:55:04 +0300

    fix: remove app controller and service boilerplate

--- js-monorepo/apps/hub-api/src/app/app.module.ts
+++ js-monorepo/apps/hub-api/src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoModule } from '@hub/todo-api';
 
 @Module({
   imports: [TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
