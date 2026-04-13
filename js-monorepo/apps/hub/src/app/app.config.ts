78a16b5a145de6afa0a1c25194dd63dd59eb7712
Yigit Soylu <ysssoylu@gmail.com>
2026-04-13 21:11:15 +0300

    refactor: move withCredentialsInterceptor to its own file

--- js-monorepo/apps/hub/src/app/app.config.ts
+++ js-monorepo/apps/hub/src/app/app.config.ts
@@ -4,17 +4,11 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  HttpInterceptorFn,
-  provideHttpClient,
-  withInterceptors,
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideEnvironment } from '@hub/ui-infra';
 import { environment } from '../environments/environment';
 import { provideNativeDateAdapter } from '@angular/material/core';
-
-const withCredentialsInterceptor: HttpInterceptorFn = (req, next) =>
-  next(req.clone({ withCredentials: true }));
+import { withCredentialsInterceptor } from '@hub/ui-infra';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- js-monorepo/libs/infra/ui/src/index.ts
+++ js-monorepo/libs/infra/ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './types/environment.interface';
 export * from './tokens/environment.token';
 export * from './services/db-wrapper.service';
+export * from './interceptors/with-credentials.interceptor';
