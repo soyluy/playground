3051e92d9cd5052120223b796b8130e089dde150
Yigit Soylu <ysssoylu@gmail.com>
2026-04-14 14:03:00 +0300

    feat: add persisting sessions across postgres

--- js-monorepo/apps/hub-api/src/main.ts
+++ js-monorepo/apps/hub-api/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
@@ -10,12 +5,20 @@ import 'dotenv/config';
 import { DEV_CORS_ORIGIN, PROD_CORS_ORIGIN } from './constants/cors.constants';
 import session from 'express-session';
 import passport from 'passport';
+import pgSession from 'connect-pg-simple';
+import { Pool } from 'pg';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global prefix
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // Port
   const port = process.env.PORT || 3000;
+
+  // CORS
   const corsOrigin =
     process.env.ENV === 'development' ? DEV_CORS_ORIGIN : PROD_CORS_ORIGIN;
   app.enableCors({
@@ -23,20 +26,36 @@ async function bootstrap() {
     credentials: true,
   });
 
+  // Session
+  const pgPool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+  const postgresSession = pgSession(session);
   if (!process.env.SESSION_SECRET) {
     throw new Error('SESSION_SECRET is not set');
   }
   app.use(
     session({
+      store: new postgresSession({
+        pool: pgPool,
+        tableName: 'session',
+        createTableIfMissing: true,
+      }),
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        secure: process.env.ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+      },
     }),
   );
 
+  // Passport
   app.use(passport.initialize());
   app.use(passport.session());
 
+  // Validation
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -47,6 +66,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Listen
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
